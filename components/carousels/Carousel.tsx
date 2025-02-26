7b1eeb7d7167f3161b6fab12c7044f2c18d449e1
Muhammad Bilal <139428118+Muhammad-Bilal-595@users.noreply.github.com>
2025-02-26 12:33:19 +0500

    Update Carousel component: replace slider names and descriptions with new content, enhance layout styles, and add animation keyframes for smoother transitions

--- components/carousels/Carousel.tsx
+++ components/carousels/Carousel.tsx
@@ -13,43 +13,51 @@ import { file, globe, img1, img2, img3 } from "@/public/images";
 const sliderData = [
   {
     img: img1,
-    name: "slider 1",
-    description: "description 1",
+    name: "Earl Moore",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce et eleifend diam. Nunc enim diam, interdum nec dui sed, pretium cursus diam. Aliquam ac accumsan sem. Aenean egestas faucibus nisi, eu malesuada nibh euismod in. Morbi fermentum lobortis nibh, scelerisque sollicitudin odio auctor et. Nam ornare nec tellus vel rhoncus. Nulla ante urna, egestas eu quam et, commodo facilisis odio. Vestibulum id venenatis tortor, eu laoreet justo. Vivamus ut ligula ac magna commodo sagittis. Proin non nisi nec massa pretium eleifend.1",
   },
   {
     img: img2,
-    name: "slider 2",
-    description: "description 2",
+    name: "Caroline Underwood",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce et eleifend diam. Nunc enim diam, interdum nec dui sed, pretium cursus diam. Aliquam ac accumsan sem. Aenean egestas faucibus nisi, eu malesuada nibh euismod in. Morbi fermentum lobortis nibh, scelerisque sollicitudin odio auctor et. Nam ornare nec tellus vel rhoncus. Nulla ante urna, egestas eu quam et, commodo facilisis odio. Vestibulum id venenatis tortor, eu laoreet justo. Vivamus ut ligula ac magna commodo sagittis. Proin non nisi nec massa pretium eleifend.2",
   },
   {
     img: img3,
-    name: "slider 3",
-    description: "description 3",
+    name: "Gertrude Haynes",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce et eleifend diam. Nunc enim diam, interdum nec dui sed, pretium cursus diam. Aliquam ac accumsan sem. Aenean egestas faucibus nisi, eu malesuada nibh euismod in. Morbi fermentum lobortis nibh, scelerisque sollicitudin odio auctor et. Nam ornare nec tellus vel rhoncus. Nulla ante urna, egestas eu quam et, commodo facilisis odio. Vestibulum id venenatis tortor, eu laoreet justo. Vivamus ut ligula ac magna commodo sagittis. Proin non nisi nec massa pretium eleifend.3",
   },
   {
     img: img1,
-    name: "slider 4",
-    description: "description 4",
+    name: "Christine Robinson",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce et eleifend diam. Nunc enim diam, interdum nec dui sed, pretium cursus diam. Aliquam ac accumsan sem. Aenean egestas faucibus nisi, eu malesuada nibh euismod in. Morbi fermentum lobortis nibh, scelerisque sollicitudin odio auctor et. Nam ornare nec tellus vel rhoncus. Nulla ante urna, egestas eu quam et, commodo facilisis odio. Vestibulum id venenatis tortor, eu laoreet justo. Vivamus ut ligula ac magna commodo sagittis. Proin non nisi nec massa pretium eleifend.4",
   },
   {
     img: globe,
-    name: "slider 5",
-    description: "description 5",
+    name: "Frederick Hansen",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce et eleifend diam. Nunc enim diam, interdum nec dui sed, pretium cursus diam. Aliquam ac accumsan sem. Aenean egestas faucibus nisi, eu malesuada nibh euismod in. Morbi fermentum lobortis nibh, scelerisque sollicitudin odio auctor et. Nam ornare nec tellus vel rhoncus. Nulla ante urna, egestas eu quam et, commodo facilisis odio. Vestibulum id venenatis tortor, eu laoreet justo. Vivamus ut ligula ac magna commodo sagittis. Proin non nisi nec massa pretium eleifend.5",
   },
   {
     img: file,
-    name: "slider 6",
-    description: "description 6",
+    name: "Nancy Nunez",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce et eleifend diam. Nunc enim diam, interdum nec dui sed, pretium cursus diam. Aliquam ac accumsan sem. Aenean egestas faucibus nisi, eu malesuada nibh euismod in. Morbi fermentum lobortis nibh, scelerisque sollicitudin odio auctor et. Nam ornare nec tellus vel rhoncus. Nulla ante urna, egestas eu quam et, commodo facilisis odio. Vestibulum id venenatis tortor, eu laoreet justo. Vivamus ut ligula ac magna commodo sagittis. Proin non nisi nec massa pretium eleifend.6",
   },
   {
     img: img3,
-    name: "slider 7",
-    description: "description 7",
+    name: "Frances Kelley",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce et eleifend diam. Nunc enim diam, interdum nec dui sed, pretium cursus diam. Aliquam ac accumsan sem. Aenean egestas faucibus nisi, eu malesuada nibh euismod in. Morbi fermentum lobortis nibh, scelerisque sollicitudin odio auctor et. Nam ornare nec tellus vel rhoncus. Nulla ante urna, egestas eu quam et, commodo facilisis odio. Vestibulum id venenatis tortor, eu laoreet justo. Vivamus ut ligula ac magna commodo sagittis. Proin non nisi nec massa pretium eleifend.7",
   },
   {
     img: img1,
-    name: "slider 8",
-    description: "description 8",
+    name: "Hannah Webb",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce et eleifend diam. Nunc enim diam, interdum nec dui sed, pretium cursus diam. Aliquam ac accumsan sem. Aenean egestas faucibus nisi, eu malesuada nibh euismod in. Morbi fermentum lobortis nibh, scelerisque sollicitudin odio auctor et. Nam ornare nec tellus vel rhoncus. Nulla ante urna, egestas eu quam et, commodo facilisis odio. Vestibulum id venenatis tortor, eu laoreet justo. Vivamus ut ligula ac magna commodo sagittis. Proin non nisi nec massa pretium eleifend.",
   },
 ];
 
@@ -65,6 +73,7 @@ const Carousel = () => {
   const [alternate, setAlternate] = useState<boolean>(true);
   const sliderLength = sliderData.length;
   const distanceDegree = 360 / numberOfWheels;
+  const [checking, setChecking] = useState<boolean>(false);
 
   // auto rotate after every 5s
   useEffect(() => {
@@ -94,6 +103,7 @@ const Carousel = () => {
     buttonClicked: "prev" | "next";
   }) => {
     const typeOfClick = buttonClicked === "prev" ? -1 : 1;
+    setChecking(() => (buttonClicked === "prev" ? true : false));
     let s = startSlice + typeOfClick;
     let e = endSlice + typeOfClick;
     s = s < 0 ? 0 : s;
@@ -117,21 +127,33 @@ const Carousel = () => {
         setStartSlice(s);
         if (e - s == numberOfWheels) setEndSlice(e);
       }
+      setChecking((prev) => !prev);
     }, 1000);
   };
 
   return (
     <section className={`Carousel--container `}>
       <div className="Information--container">
-        <div className="">
+        <div className="w-full flex justify-end items-center flex-col lg:gap-8  max-lg:flex-center md:min-h-[60%] h-full bg-white px-10">
           <Image
+            id="image"
             src={sliderData[sliderIndex].img}
             alt=""
-            className={`w-[100px] h-auto ${isClicked ? "object-[100px_60px]" : "bg-black object-[0px_0px]"} transition-all duration-500  rounded-full p-2 flex-center`}
+            className={`w-[250px] md:w-[350px] h-auto ${
+              isClicked
+                ? checking
+                  ? "object-[-300px_60px] blur-[10px]  "
+                  : "object-[300px_100px] blur-[10px] "
+                : "bg-black object-[0px_0px] "
+            } bg-slate-400 ring-black/50 ring-4 blur-0  transition-all duration-500  rounded-full p-2 flex-center `}
           />
-          <div>Name:{sliderData[sliderIndex].name}</div>
+          <div className="flex-center text-black md:text-xl font-semibold bg-orange-200 lg:h-[10%] w-full">
+            <h3 className="">{sliderData[sliderIndex].name}</h3>{" "}
+          </div>
         </div>
-        <div>{sliderData[sliderIndex].description}</div>
+        <p className="w-full bg-stone-400 h-full text-sm max-md:p-2 p-10 text-justify max-h-[30%] overflow-auto">
+          {sliderData[sliderIndex].description}
+        </p>
       </div>
       <div className="Carousel_Controler--container ">
         {/* wheel container */}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,26 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      keyframes: {
+        wheel: {
+          
+          to: {
+            objectPosition: "0px 0px",
+          },
+        },
+        wheel_right: {
+          "0%": {
+            objectPosition: "-100px 60px",
+          },
+          "100%": {
+            objectPosition: "0px 0px",
+          },
+        },
+      },
+      animation: {
+        wheel: "wheel 1s ease-in forwards",
+        wheel_right: "wheel_right 1s cubic-bezier(0.4, 0, 0.2, 1.2) forwards",
+      },
     },
   },
   plugins: [],
